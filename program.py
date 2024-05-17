5dc98ff36bf874b07bd82e7ac461ac45c0fb787e
Tapolcsányi Attila <67805513+VeneVrks@users.noreply.github.com>
2024-05-17 14:46:29 +0200

    program
    program

--- program.py
+++ program.py
@@ -2,11 +2,15 @@ from abc import ABC, abstractmethod
 import os
 import time
 import re
-
+import random
+from datetime import datetime, date, timedelta
 
 class Szoba(ABC):
     ar = 0
     szobaszam = 0
+
+    def __init__(self, szobaszam):
+        self.szobaszam = szobaszam
 
 class EgyagyasSzoba(Szoba):
     ar = 1500
@@ -20,7 +24,19 @@ class Szalloda:
     nev = ""
     szobak = []
 
-    def __init__(self, nev, szobak):
+    def __init__(self, nev):
+        szobak = []
+        for i in range(0, random.randint(3, 10)):
+            egyagyas = random.randint(0, 1) == 1
+            
+            szoba = False
+            if egyagyas:
+                szoba = EgyagyasSzoba(i)
+            else:
+                szoba = KetagyasSzoba(i)
+            
+            szobak.append(szoba)
+
         self.nev = nev
         self.szobak = szobak
 
@@ -44,30 +60,56 @@ class Foglalas:
 
 
 
-szalloda = Szalloda("Teszt Szálloda", [])
+szalloda = Szalloda("Teszt Szálloda")
 
 
 
 foglalasok = []
-def foglalasLemondas(szobaszam):
-    for i in range(1, len(foglalasok)):
-        foglalas = foglalasok[i]
+def foglalasLemondas(foglalasID):
+    if not foglalasID:
+        return False
+    
+    foglalasok.pop(int(foglalasID))
+    return True
 
-        if foglalas and foglalas.szobaszam == szobaszam:
-            foglalasok.pop(i)
+def foglalasHozaadasa(nev, szobaszam, datum):
+    if not nev or not szobaszam or not datum:
+        return False
+    
+    adat = {
+        'nev': nev,
+        'szobaszam': szobaszam,
+        'datum': datum,
+        'ar': szalloda.szobak[szobaszam].ar,
+        'agyakSzama': szalloda.szobak[szobaszam].agyakSzama,
+    }
+    foglalasok.append(adat)
+    
+    return szalloda.szobak[szobaszam].ar
 
-            return True
+def foglalasokListazasa(showID):
+    if len(foglalasok) == 0:
+        print('Nincs jelenleg egy foglalás sem!')
+        return
 
-    return False
-
-def foglalasokListazasa():
+    foglalasID = 1
     for foglalas in foglalasok:
-        print(foglalas.szobaszam)
+        if showID:
+            print(f'foglalásID: {foglalasID}, szobaszám: {foglalas['szobaszam']}, foglalva: {foglalas['datum']}, név: {foglalas['nev']}, ágyak száma: {foglalas['agyakSzama']}, ár: {foglalas['ar']}Ft')
+        else:
+            print(f'szobaszám: {foglalas['szobaszam']}, foglalva: {foglalas['datum']}, név: {foglalas['nev']}, ágyak száma: {foglalas['agyakSzama']}, ár: {foglalas['ar']}Ft')
+
+        foglalasID += 1
+
+foglalasHozaadasa('Kis Pista', 1, '2025-10-10')
+foglalasHozaadasa('Kis Pista', 1, '2025-10-13')
+foglalasHozaadasa('Kis Pista', 2, '2025-10-10')
 
 
 
 
-panels = ['foglalás', 'lemondás', 'listázás']
+
+panels = ['foglalás', 'lemondás', 'listázás', 'kilépés']
 def clearPanel():
     if os.name == 'nt':
         os.system('cls')
@@ -76,36 +118,115 @@ def clearPanel():
 
 def getInput(text, numberOnly):
     value =""
-    while value == "" and (numberOnly and not re.match(r"^[0-9]+$", value)):
+    while value == "" or (numberOnly and not re.match(r"^[0-9]+$", value)):
         value = input(text)
 
     return value
 
-panel = 0
-def drawPanel():
+def drawPanel(panel):
     clearPanel()
 
-    if panel == 0:
+    if panel == 0: # Főmenü
+        print(f"Üdvözöljük a {szalloda.nev} recepciós rendszerébe.")
+
+        text = ""
         for i in range(0, len(panels)):
             name = panels[i]
             if not name:
                 continue
-
-            print(f'{name} {i}')
-
+            
+            text += f'{name}: {i+1} '
+        print(text)
         
-        value = getInput('Válassz menü-t: ', True)
+        panelID = int(getInput('Válassz menü-t: ', True))
+        while panelID < 1 or panelID > len(panels):
+            panelID = int(getInput('Válassz menü-t: ', True))
 
-    else:
-        print("")
+        drawPanel(panelID)
+    elif panel == 1: # Foglalás
+        print("Szobák: ")
+        for szoba in szalloda.szobak:
+            print(f'  szobaszám: {szoba.szobaszam}, ár: {szoba.ar}Ft, Ágyak száma: {szoba.agyakSzama}')
+        
+        nev = getInput('Adja meg a nevét: ', False)
+        while not re.match(r'[A-Za-z]+ [A-Za-z]+', nev):
+            nev = getInput('Adja meg a nevét: ', False)
+
+        szobaszabad = False
+        while not szobaszabad:
+            szobaszam = int(getInput('Válassz szobaszámot: ', True))
+            while szobaszam < 0 or szobaszam > len(szalloda.szobak) - 1:
+                szobaszam = int(getInput('Válassz szobaszámot: ', True))
+
+            datum = getInput('Válassz dátumot: ', False)
+            while not re.match(r"\b(19\d\d|20\d\d)[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12]\d|3[01])\b", datum) or datetime.strptime(datum, '%Y-%m-%d').date() < date.today():
+                datum = getInput('Válassz dátumot: ', False)
+
+            talalat = False
+            for foglalas in foglalasok:
+                if foglalas['szobaszam'] == szobaszam:
+                    foglaltDatum = datetime.strptime(foglalas['datum'], '%Y-%m-%d').date()
+                    bekertDatum = datetime.strptime(datum, '%Y-%m-%d').date()
+
+                    if foglaltDatum == bekertDatum or foglaltDatum + timedelta(days=1) == bekertDatum or bekertDatum - timedelta(days=-1) == foglaltDatum:
+                        talalat = True
+                        break
+
+            szobaszabad = not talalat
+
+            if not szobaszabad:
+                print('Ez a szoba már levan foglalva erre az időpontra!')
+                time.sleep(1)
+
+        result = foglalasHozaadasa(nev, szobaszam, datum)
+        if result:
+            print('Foglalás sikeres!')
+            print(f'Szoba ára: {result}Ft')
+
+        time.sleep(2)
+        drawPanel(0)
+    elif panel == 2: # Lemondás
+        foglalasokListazasa(True)
+        print('Visszalépéshez irja be a 0-át.')
+
+        foglalasID = int(getInput('Válassz foglalást!: ', True))
+        while foglalasID < 0 or foglalasID > len(foglalasok):
+            foglalasID = int(getInput('Válassz foglalást!: ', True))
+
+        if foglalasID == 0:
+            drawPanel(0)
+
+        foglalasID -= 1
+        foglalas = foglalasok[foglalasID]
+        print(f'szobaszám: {foglalas['szobaszam']}, foglalva: {foglalas['datum']}, ágyak száma: {foglalas['agyakSzama']}, ár: {foglalas['ar']}Ft')
+        
+        value = getInput('Biztosan ezt szeretné törölni? (igen, nem) ', False)
+        while value != 'igen' and value != 'nem':
+            value = getInput('Biztosan ezt szeretné törölni? (igen, nem) ', False)
+
+        if value == 'igen':
+            foglalasLemondas(foglalasID)
+            drawPanel(0)
+        else:
+            drawPanel(2)
+    elif panel == 3: # Listázás
+        foglalasokListazasa(False)
+        print('visszalépés: 0')
+
+        value = int(getInput('Válassz menü-t: ', True))
+        while value != 0:
+            value = int(getInput('Válassz menü-t: ', True))
+
+        panel = value
+        drawPanel(panel)
+    elif panel == 4: # Kilépés
+        print("Viszont látásra!")
+        time.sleep(2)
+        clearPanel()
+        exit()
 
 
 def Main():
-    drawPanel()
-
-
-
-
-
+    drawPanel(0)
 
 Main()
